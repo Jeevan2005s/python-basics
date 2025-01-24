71c5eed0245e51fcf4420f15ee67b49474289619
Jeevan2005s <jeevanprasad2005s@gmail.com>
2025-01-24 19:32:19 +0530

    delete old

--- python/inherit.py
+++
@@ -1,9 +0,0 @@
-class students:
-    def details(self):
-        print(self.name)
-    def __init__(self,name):
-        self.name=name 
-class det(students):
-    pass
-o=det("jeevan")
-o.details()      
--- python/problem1.py
+++
@@ -1,9 +0,0 @@
-x=input("")
-y=input("")
-z=0
-for i in range(len(x)):
-    x=x[-1]+x[0:len(x)-1]
-    for j in range(len(x)):
-        if x[j:j+len(y)]==y: 
-            z+=j   
-print(z)
