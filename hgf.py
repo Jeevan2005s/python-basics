98ac46ba0ec273125b967e81de0f1d903928c98e
Jeevan2005s <jeevanprasad2005s@gmail.com>
2025-01-24 20:30:16 +0530

    2nd today

--- hgf.py
+++ hgf.py
@@ -3,3 +3,4 @@ x=[{"name":"soap","rate":50},
    {"name":"rice","rate":90}]
 b=list(map(lambda x:{"name":x["name"],"rate":x["rate"]-(x["rate"]/100)*25} if x["rate"]>100 else x,x))
 print(b)
+print("jgjh")
