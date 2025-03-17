0f5acb182bf398710ac645c6613881b4044d24df
Jeevan2005s <jeevanprasad2005s@gmail.com>
2025-03-17 20:35:41 +0530

    fibonacci

--- fibonacciseries.py
+++ fibonacciseries.py
@@ -13,10 +13,12 @@ print(*fibonacci(0,1,s,[]))
 # def fib(a):
 #     l=[0,1]
 #     i,j=0,1
+#     if a==0 or a==1:
+#         return l[a]
 #     while len(l)<=a:
 #         l.append(l[i]+l[j])
 #         i+=1
 #         j+=1
-#     return l
+#     return l[-1]
 # a=int(input("Enter the number of elements you want:"))
-# print(*fib(5))    
+# print(fib(a))    
