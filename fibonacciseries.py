a7696d8e6126b35c1116366b82f326691932529e
Jeevan2005s <jeevanprasad2005s@gmail.com>
2025-03-18 19:21:37 +0530

    modified fibonacciseries

--- fibonacciseries.py
+++ fibonacciseries.py
@@ -1,24 +1,25 @@
-def fibonacci(i,j,a,y):
-    if a==0:
-        return y
-    y.append(i)
-    i,j=j,i+j
-    a-=1
-    fibonacci(i,j,a,y)
-    return y
-s=int(input("Enter the number of elements you want:"))
-print(*fibonacci(0,1,s,[]))
+# #own code
+# def fibonacci(i,j,a,y):
+#     if a==0:
+#         return y
+#     y.append(i)
+#     i,j=j,i+j
+#     a-=1
+#     fibonacci(i,j,a,y)
+#     return y
+# s=int(input("Enter the number of elements you want:"))
+# print(*fibonacci(0,1,s,[]))
 
+#modified code
 
-# def fib(a):
-#     l=[0,1]
-#     i,j=0,1
-#     if a==0 or a==1:
-#         return l[a]
-#     while len(l)<=a:
-#         l.append(l[i]+l[j])
-#         i+=1
-#         j+=1
-#     return l[-1]
-# a=int(input("Enter the number of elements you want:"))
-# print(fib(a))    
+def fibonac(n):
+    if n==0:
+        return "invalid"
+    elif n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonac(n-1)+fibonac(n-2)    
+print(fibonac(9))
+   
