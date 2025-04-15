972f900c0bb1aae8021e4c478ad069ce6e599cf4
Jeevan2005s <jeevanprasad2005s@gmail.com>
2025-04-15 20:06:11 +0530

    linkde list

--- linkedlist.py
+++ linkedlist.py
@@ -3,8 +3,6 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
-        print(self.data)
-        print(self.next)
 class LinkedList:
     def __init__(self):
         self.head=None
@@ -17,16 +15,48 @@ class LinkedList:
         while i.next:
             i=i.next
         i.next=new_node
-
+    def remove(self,data):
+        i=self.head
+        j=None
+        while i:
+            if i.data==data:
+                if j:
+                    j.next=i.next
+                else:
+                    self.head=i.next
+            p=i
+            i=i.next            
+    def insert(self,data,pos):
+        new_node=Node(data)
+        if pos==0:
+            new_node.next=self.head
+            self.head=new_node
+            return
+        i=self.head
+        for j in range(pos-1):
+            i=i.next
+        new_node.next=i.next
+        i.next=new_node    
     def display(self):
         print("display")
         i=self.head
         while i:
             print(i.data)
-            i=i.next        
+            i=i.next 
+    def length(self):
+        count=0
+        i=self.head
+        while i:
+            i=i.next
+            count+=1
+        print("length of the linked list is",count)                   
 
 l=LinkedList()
 l.append(1)
 l.append(2)
 l.append(3)
 l.display()
+l.insert(5,3)
+l.display()
+l.length()
+
